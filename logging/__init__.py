ac95534eca88e5724b30e786c1969b3a8a001ffa
stkyle <stkyle@users.noreply.github.com>
2015-04-07 07:13:49 -0400

    Update __init__.py

--- logging/__init__.py
+++ logging/__init__.py
@@ -1,7 +1,52 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=W0212
-"""
-Log Forging
+"""Patch for Python Standard Library module `logging'
+
+**Log Forging**
+
+Log Forging is considered an OSWAP `Vulnerability`
+
+
+Writing unvalidated user input to log files can allow an attacker to forge log
+entries or inject malicious content into the logs.
+
+Log forging vulnerabilities occur when:
+
+    * Data enters an application from an untrusted source.
+    * The data is written to an application or system log file.
+
+Applications typically use log files to store a history of events or 
+transactions for later review, statistics gathering, or debugging. Depending
+on the nature of the application, the task of reviewing log files may be 
+performed manually on an as-needed basis or automated with a tool that
+automatically culls logs for important events or trending information.
+
+Interpretation of the log files may be hindered or misdirected if an attacker
+can supply data to the application that is subsequently logged verbatim. In 
+the most benign case, an attacker may be able to insert false entries into the
+log file by providing the application with input that includes appropriate 
+characters. If the log file is processed automatically, the attacker can 
+render the file unusable by corrupting the format of the file or injecting 
+unexpected characters. A more subtle attack might involve skewing the log file 
+statistics. Forged or otherwise, corrupted log files can be used to cover an 
+attacker's tracks or even to implicate another party in the commission of a 
+malicious act. In the worst case, an attacker may inject code or other 
+commands into the log file and take advantage of a vulnerability in the log 
+processing utility. 
+
+:Example:
+
+The following web application code attempts to read an integer value from a 
+request object. If the value fails to parse as an integer, then the input is 
+logged with an error message indicating what happened.
+
+If a user submitted string having the value "twenty-one" for val, the 
+following entry is logged:
+
+Attackers can use this same mechanism to insert arbitrary log entries. 
+
+`Reference: <https://www.owasp.org/index.php/Log_Forging>`_
+
 https://www.owasp.org/index.php/Log_Forging
 https://cwe.mitre.org/data/definitions/117.html
 
